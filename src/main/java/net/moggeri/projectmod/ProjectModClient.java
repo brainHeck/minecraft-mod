388d752afdce6b79084c04edbcb580d3980ba4db
foo <beastiefish@disroot.org>
2024-06-08 15:15:04 +0200

    Project setup.

---
+++ src/main/java/net/moggeri/projectmod/ProjectModClient.java
@@ -0,0 +1,10 @@
+package net.moggeri.projectmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/moggeri/projectmod/ProjectModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.moggeri.projectmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ProjectModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
